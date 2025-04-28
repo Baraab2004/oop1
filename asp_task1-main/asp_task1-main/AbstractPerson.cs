21b2885ccd5cb6ba817b3600ea2fc339e96968a1
Baraab2004 <156257765+Baraab2004@users.noreply.github.com>
2025-04-28 13:55:29 +0300

    Add files via upload

---
+++ asp_task1-main/asp_task1-main/AbstractPerson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    abstract class AbstractPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> BorrowedBooks { get; set; }
+        public AbstractPerson(int id,string name)
+        {
+            Id = id;
+            Name = name;
+            BorrowedBooks = new List<Book>();
+        }
+
+        abstract public void DisplayInfo();
+
+    }
+}
